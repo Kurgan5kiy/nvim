d566f776888e9327da09dd6bcb8ff696b63be195
beholder <kurganskiyvladislav@gmail.com>
2026-04-29 16:37:35 +0200

    Add Ctlr+H,J,K,L bindings to move b/w windows.

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -5,6 +5,12 @@
 -- Neotree toggle
 vim.keymap.set("n", "<leader>e", "<Cmd>Neotree toggle<CR>")
 
--- Delete withou yanking
+-- Delete without yanking
 vim.keymap.set("n", "dd", '"_dd')
 vim.keymap.set("v", "d", '"_d')
+
+-- Window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
